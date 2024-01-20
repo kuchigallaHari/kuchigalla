2c261280b84e278372b7658b55ed71237c8f458c
srianji9090 <you@harikrishnakuchi1998@gmail.com>
2024-01-20 20:49:17 +0530

    bye

---
+++ SprinBoot-practice-3/src/main/java/com/test/SprinBootPractice3Application.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootPractice3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBootPractice3Application.class, args);
+	}
+
+}
---
+++ SprinBoot-practice-3/src/test/java/com/test/SprinBootPractice3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinBootPractice3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
