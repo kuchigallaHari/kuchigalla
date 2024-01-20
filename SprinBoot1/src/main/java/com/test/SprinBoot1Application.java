d8c11a650e3f8c74263985af72868d9a65a439a1
srianji9090 <you@harikrishnakuchi1998@gmail.com>
2024-01-20 14:24:49 +0530

    Hello

---
+++ SprinBoot1/src/main/java/com/test/SprinBoot1Application.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBoot1Application.class, args);
+		System.out.println("Done..");
+		
+		
+		
+	}
+
+}
